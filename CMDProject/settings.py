6324f3f564ae45b9daf23e2a159395430bfdd04c
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-06-30 14:48:35 +0100

    added bootstrap template

--- CMDProject/settings.py
+++ CMDProject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_FILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'CMDProject/static')
 ]
 
