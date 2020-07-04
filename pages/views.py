a9761a75edcd52494eb542f4d68d0392ae9e4e96
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-04 18:50:23 +0100

    adapted the team section to accept dynamic content for easy modifications and updates

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from realtors.models import Realtor
+from realtors.models import Realtor, TeamSection
 
 def index(request):
     realtors = Realtor.objects.all()
+    teamsection = TeamSection.objects.all()
     context = {
-        'realtors': realtors
+        'realtors': realtors,
+        'teamsection': teamsection
     }
     return render(request, 'pages/index.html', context)
 
--- realtors/models.py
+++ realtors/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+class TeamSection(models.Model):
+    team_title = models.CharField(max_length=200)
+    team_subtitle = models.CharField(max_length=200)
+    team_description = models.TextField(blank=True)
+
+
+
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
+    job = models.CharField(max_length=20, null=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
