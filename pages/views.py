d30307719ca494b9b5da05e53efce01bbe75e32b
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-04 17:46:17 +0100

    fixed bug with team members not displaying on template

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from realtors.models import Realtor
+
 def index(request):
-    return render(request, 'pages/index.html')
+    realtors = Realtor.objects.all()
+    context = {
+        'realtors': realtors
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
--- realtors/models.py
+++ realtors/models.py
@@ -6,7 +6,6 @@ class Realtor(models.Model):
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
-    is_mvp = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
