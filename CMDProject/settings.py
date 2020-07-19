0e6337651e04f93ef01289eec6e1553b69f04f23
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-19 14:24:39 +0100

    added settings for heroku

--- CMDProject/settings.py
+++ CMDProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,13 +79,9 @@ WSGI_APPLICATION = 'CMDProject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cmddb',
-        'USER': 'postgres',
-        'PASSWORD': 'marius123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
@@ -139,5 +136,5 @@ MEDIA_URL = '/media/'
 
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
