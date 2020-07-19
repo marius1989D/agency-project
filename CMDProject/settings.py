4f0bcb567de9cdf06dfeece8fec8b241d3437a39
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-19 14:04:21 +0100

    added settings for heroku

--- CMDProject/settings.py
+++ CMDProject/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CMDProject.urls'
@@ -128,6 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'CMDProject/static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # MEDIA FOLDER SETTINGS
