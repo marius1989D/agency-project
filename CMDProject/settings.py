60b81acdba43fcb535c69d4d928b17f82d3e1416
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-19 14:13:04 +0100

    added settings for heroku

--- CMDProject/settings.py
+++ CMDProject/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
         'NAME': 'cmddb',
         'USER': 'postgres',
         'PASSWORD': 'marius123',
-        'HOST': 'localhost'
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
