062c953aac1227721c22c20092ae7e39e4f43b88
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-07-19 14:00:26 +0100

    added settings for heroku

--- CMDProject/settings.py
+++ CMDProject/settings.py
@@ -133,3 +133,8 @@ STATICFILES_DIRS = [
 # MEDIA FOLDER SETTINGS
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
